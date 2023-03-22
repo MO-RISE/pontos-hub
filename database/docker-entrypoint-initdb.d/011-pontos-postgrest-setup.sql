3368d78e236e79f002f063b4361d9b9440b1b0da
Fredrik Olsson <freol@outlook.com>
2023-03-22 12:36:57 +0100

    Using a master table with views and including some example data. Adding a web_user for further work with authentication solution.

--- database/docker-entrypoint-initdb.d/011-pontos-postgrest-setup.sql
+++ database/docker-entrypoint-initdb.d/011-pontos-postgrest-setup.sql
@@ -1,13 +1,20 @@
--- Anonymuous role for unautenticated logins
-CREATE ROLE web_anon nologin;
+-- Setting up views and roles for use with PostgREST
 
--- Allow usage on schema vessel_data to the web_anon role
-GRANT USAGE ON SCHEMA vessel_data TO web_anon;
+CREATE SCHEMA api_views;    -- Will contain specific views of the data that should be accessible to the postgREST api users
 
--- This gives the web_anon role select rights to all future tables in the vessel_data schema created by pontos_user
-ALTER DEFAULT PRIVILEGES FOR USER pontos_user IN SCHEMA vessel_data GRANT SELECT ON TABLES TO web_anon;
+CREATE ROLE web_anon nologin; -- Anonymuous role for unautenticated logins
+CREATE ROLE web_user nologin; -- Authenticated role
+
+-- Allow usage on schema api_views to all roles
+GRANT USAGE ON SCHEMA api_views TO web_anon;
+GRANT USAGE ON SCHEMA api_views TO web_user;
+
+-- This gives the specified role select rights to all future tables in the api_views schema created by pontos_user
+ALTER DEFAULT PRIVILEGES FOR USER pontos_user IN SCHEMA api_views GRANT SELECT ON TABLES TO web_anon;
+ALTER DEFAULT PRIVILEGES FOR USER pontos_user IN SCHEMA api_views GRANT SELECT ON TABLES TO web_user;
 
 
--- Create authenticator role for postgrest connection and make sure it can morph into the web_anon role
+-- Create authenticator role for postgrest connection and make sure it can morph into the other roles
 CREATE ROLE authenticator noinherit LOGIN PASSWORD 'authenticator-password';
 GRANT web_anon TO authenticator;
+GRANT web_user TO authenticator;
