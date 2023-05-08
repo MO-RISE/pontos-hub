88a2ae0897462c4d1116830bc4ef9d261584ab77
Fredrik Olsson <freol@outlook.com>
2023-05-08 13:17:45 +0200

    Adding a tiny jwt issuer application in bash (exposed on /token)

--- .devcontainer/install-utility-scripts.sh
+++ .devcontainer/install-utility-scripts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-## jwt-cli
-wget -qO- https://github.com/mike-engel/jwt-cli/releases/download/5.0.3/jwt-linux.tar.gz \
-    | sudo tar xvz -C /usr/local/bin \
-    && sudo chmod +x /usr/local/bin/jwt
+# Install shell2http
+wget -q -O - https://github.com/msoap/shell2http/releases/download/v1.16.0/shell2http_1.16.0_linux_amd64.tar.gz | sudo tar xvz -C /usr/local/bin shell2http
+
+# Install jwt-cli
+wget -q -O - https://github.com/mike-engel/jwt-cli/releases/download/5.0.3/jwt-linux.tar.gz | sudo tar xvz -C /usr/local/bin jwt
